612b5f9498fcd8d81c40e86ef02a67464d935a4e
Robert Juliano <rovjuvano@gmail.com>
2014-04-23 02:25:01 +0000

    uncommitted work in progress

--- src/index.js
+++ src/index.js
@@ -1,89 +1,215 @@
 var http = require('http'),
 	Rx = require('rx');
 
-var server = http.createServer();
-var observable = Rx.Observable.fromEvent(server, 'request', function(args) {
-	return {request: args[0], response: args[1]};
-})
-observable = catchError(observable, observable
-	.select(makeConfig)
-	.select(requireMethod('POST'))
-	.selectMany(readBody)
-	.select(parseJson)
-);
-observable.subscribe(
-	function(x) {
-		x.response.end();
-		console.info(x.config);
-	},
-	function(x) {
-		x.response.statusCode = x.error.statusCode || 500;
-		x.response.write(http.STATUS_CODES[ x.response.statusCode ])
-		x.response.write(': ');
-		x.response.write(x.error.name + ': ' + x.error.message);
-		x.response.end();
-		console.error(x.error, x.error.stack);
-	},
-	function() {
-		console.log('Nothing to see here');
+function makeRouter() {
+	function router(request) {
+		return route(request)(request);
 	}
-);
-server.listen(3000);
-
-function makeConfig(x) {;
-	x.config = {
-		method: x.request.method,
-		url: x.request.url,
-		headers: x.request.headers,
+	var routes = [];
+	var defaultRoute;
+	router.when = function(fn, app) {
+		routes.push({fn: fn, app: app});
+		return this;
 	};
-	return x;
-};
-function readBody(x) {
-	return Rx.Observable.create(function(observer) {
-		var data = '';
-		x.request.setEncoding('utf8');
-		x.request.on('data', function (chunk) {
-			data += chunk;
-		});
-		x.request.on('end', function() {
-			x.config.data = data;
-			observer.onNext(x);
-			observer.onCompleted();
-		});
-		x.request.on('error', function(err) {
-			observer.onError(err);
-		});
-	});
-};
-function parseJson(x) {
-	try {
-		x.config.data = JSON.parse(x.config.data);
-	} catch(e) {
-		e.statusCode = 400;
-		throw e;
-	}
-	return x;
-};
-function requireMethod(method) {
-	return function(x) {
-		if (x.config.method !== method) {
-			var error = new Error(http.STATUS_CODES[405]);
-			error.statusCode = 405;
-			throw error;
+	router.otherwise = function(app) {
+		defaultRoute = app;
+		return this;
+	};
+	function route(request) {
+			console.warn(routes);
+		for (var i=0, len=routes.length; i<len; ++i) {
+			console.warn(routes[i]);
+			if (routes[i].fn(request)) {
+				return routes[i].app;
+			}
 		}
-		return x;
+		return defaultRoute || default404;
+	}
+	function default404(request) {
+		var bodyString = '404 Not Found';
+		return Rx.Observable.return({
+			status: 404,
+			headers: {
+				'Content-Type': 'text/plain',
+				'Content-Length': Buffer.byteLength(bodyString),
+			},
+			body: Rx.Observable.return(bodyString)
+		});
+	}
+	return router;
+};
+
+function app1(request) {
+	return Rx.Observable.return({
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: Rx.Observable.return(['This', 'is', 'JSON', {a:1, b:2}, true]),
+	});
+}
+var app2 = makeRouter()
+	.when(function(request) {
+		return /123/.test(request.url);
+	}, app2a)
+	.when(function(request) {
+		return /987/.test(request.url);
+	}, app2b)
+;
+function app2a(request) {
+	var bodyString = 'This is 2a';
+	return Rx.Observable.return({
+		status: 200,
+		headers: {
+			'Content-Type': 'text/html',
+			'Content-Length': Buffer.byteLength(bodyString),
+		},
+		body: Rx.Observable.return(bodyString),
+	});	
+};
+function app2b(request) {
+	var bodyString = 'This is 2b';
+	return Rx.Observable.return({
+		status: 200,
+		headers: {
+			'Content-Type': 'text/html',
+			'Content-Length': Buffer.byteLength(bodyString),
+		},
+		body: Rx.Observable.return(bodyString),
+	});	
+};
+
+var router = makeRouter()
+.when(function(request) {
+	return /abc/.test(request.url);
+}, jsonify(app1))
+.when(function(request) {
+	return /xyz/.test(request.url);
+}, basicAuth(app2))
+;
+
+Rx.Observable.return(router).subscribe(startApp);
+
+function basicAuth(app) {
+	return function(request) {
+		var authorizationHeader = getAuthorizationHeader(request);
+		if (authorizationHeader) {
+			var credentials = parseCredentials(authorizationHeader);
+			if (validCredentials(credentials)) {
+				return app(request);
+			}
+		}
+		var authorizationRequiredResponse = {
+			status: 401,
+			headers: {
+				'Content-Type': 'text/plain',
+				'WWW-Authenticate': 'Basic',
+			},
+			body: Rx.Observable.return('Authorization Required.')
+		};
+		return Rx.Observable.return(authorizationRequiredResponse);
+	};
+	function getAuthorizationHeader(request) {
+		return request.headers['authorization'];
+	};
+	function parseCredentials(string) {
+		var matches = string.match(/Basic (.*)/);
+		if (matches) {
+			var parts = base64decode(matches[1]).split(':');
+			return {
+				username: parts[0],
+				password: parts[1]
+			};
+		}
+		return;
+	}
+	function base64decode(string) {
+		return new Buffer(string, 'base64').toString('utf-8');
+	}
+	function validCredentials(credentials) {
+		return credentials && credentials.username === credentials.password;
+	}
+}
+function jsonify(app) {
+	return function(request) {
+		return app(request).selectMany(jsonifyResponse);
+	};
+	function jsonifyResponse(response) {
+		if (response.headers['Content-Type'] && response.headers['Content-Type'] === 'application/json') {
+			return response.body.single().map(jsonifyObject);
+		}
+		else {
+			return Rx.Observable.return(response);
+		}
+		function jsonifyObject(object) {
+			var bodyString = JSON.stringify(object);
+			response.body = Rx.Observable.return(bodyString);
+			response.headers['Content-Length'] = bodyString.length;
+			return response;
+		}
 	}
 };
-function catchError(obs1, obs2) {
-	return Rx.Observable.zip(obs1, obs2.catch(function(err) {
-		return Rx.Observable.return(err);
-	}), function(x, y) {
-		if (y instanceof Error) {
-			x.error = y;
-			throw x;
+function startApp(app) {
+	http.createServer(handleRequest).listen(3000);
+	function handleRequest(nodeRequest, nodeResponse) {
+		console.info('** handling request...');
+		try {
+			var jsackRequest = jsackRequestFromNodeRequest(nodeRequest);
+			var responseObserver = makeResponseObserver(nodeResponse);
+			app(jsackRequest).subscribe(responseObserver);
+		} catch(error) {
+			console.error('** request completed very abnormally', error.stack);
+			var bodyString = 'Internal Server Error. Fire all the developers!';
+			var jsackResponse = {
+				status: 500,
+				headers: {
+					'Content-Type': 'text/plain',
+					'Content-Length': Buffer.byteLength(bodyString),
+				},
+				body: Rx.Observable.return(bodyString)
+			};
+			sendResponse(jsackResponse, nodeResponse);
 		}
-		return y;
-	});
+	};
+	function makeResponseObserver(nodeResponse) {
+		return {
+			onNext: function(jsackResponse) {
+				sendResponse(jsackResponse, nodeResponse);
+			},
+			onError: function(error) {
+				console.error('** request completed abnormally', error.stack);
+				var bodyString = error.name + ': ' + error.message;
+				var jsackResponse = {
+					status: error.httpStatusCode || 500,
+					headers: {
+						'Content-Type': 'text/plain',
+						'Content-Length': Buffer.byteLength(bodyString),
+					},
+					body: Rx.Observable.return(bodyString)
+				};
+				sendResponse(jsackResponse, nodeResponse);
+			},
+			onCompleted: function() {
+				console.info('** request completed normally');
+			}
+		};
+	}
 };
-// jsack: return [statusCode:Integer, body:Rx.Observable, headers:Object]
-// jsack-application: return Rx.Observable.return({status: Integer, body: Rx.Observable, headers: Object});
+function jsackRequestFromNodeRequest(nodeRequest) {
+	return nodeRequest;
+};
+function sendResponse(jsackResponse, nodeResponse) {
+	nodeResponse.writeHeader(jsackResponse.status, jsackResponse.headers);
+	jsackResponse.body.subscribe(
+		function(chunk) {
+			nodeResponse.write(chunk);
+		},
+		function(error) {
+			console.error('** sending response completed abnormally', error.stack);
+			nodeResponse.setTimeout(1);
+		},
+		function() {
+			nodeResponse.end();
+		}
+	);
+};
